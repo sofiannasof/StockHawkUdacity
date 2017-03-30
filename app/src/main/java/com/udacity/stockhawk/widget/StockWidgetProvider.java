27f8aac5801c6dd0d9d066376ed0eb8ebf7d99ef
sofiannasof <s0fianna.menesidou@gmail.com>
2017-03-30 20:28:15 +0300

    style: color

--- app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
@@ -48,7 +48,7 @@ public class StockWidgetProvider extends AppWidgetProvider {
                 remoteViews.setPendingIntentTemplate(R.id.widget_list, pendingIntentTemplate);
                 remoteViews.setEmptyView(R.id.widget_list, R.id.widget_empty);
                 remoteViews.setInt(R.id.widget_list, "setBackgroundResource", R.color.colorPrimary);
-                remoteViews.setInt(R.id.widget_content, "setBackgroundResource", R.color.colorPrimary);
+                remoteViews.setInt(R.id.widget_content, "setBackgroundResource", R.color.white);
                 remoteViews.setContentDescription(R.id.widget_list, context.getString(R.string.widget));
                 appWidgetManager.updateAppWidget(widgetId, remoteViews);
             }
